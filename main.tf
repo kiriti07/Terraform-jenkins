1233939339f3e850f7b50520ba1dfada423da022
kiriti07 <ch.kiriti@gmail.com>
2023-05-09 18:50:39 +0530

    Adding terraform provider

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
-  version = "~> 4.0"
-  source = "hashicorp/aws"
 }
 
 resource "aws_instance" "aws_sample_jenkins" {
