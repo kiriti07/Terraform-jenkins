4be3b162154339639e76921bb77bf9135138435a
kiriti07 <ch.kiriti@gmail.com>
2023-05-09 16:24:12 +0530

    adding required_providers

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
-    version = "~> 4.0"
     region = "ap-south-1"  
 }
 
